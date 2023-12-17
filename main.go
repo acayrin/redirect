f016967475de397ec2bb1d6a287abe1ff491496c
acayrin <ow3kf1zge@mozmail.com>
2023-12-18 01:26:04 +0700

    new

--- main.go
+++ main.go
@@ -6,199 +6,135 @@ import (
 	"io/ioutil"
 	"math/rand"
 	"net/http"
-	"os"
 	"strings"
 	"time"
 )
 
-type Service struct {
-	Type      string   `json:"type"`
-	TestURL   string   `json:"test_url"`
-	Fallback  string   `json:"fallback"`
-	Instances []string `json:"instances"`
+type AppConfig struct {
+	ServiceListURL string
+	Interval       int
 }
 
-var services []Service
-var serviceStates map[string][]struct {
-	Server    string `json:"server"`
-	Available bool   `json:"available"`
+type ServiceType struct {
+	Type      string
+	TestURL   string
+	Fallback  string
+	Instances []string
 }
 
-func fetchWithTimeout(url string, timeout time.Duration) (*http.Response, error) {
-	client := http.Client{
-		Timeout: timeout,
-	}
-	return client.Head(url)
+type ServiceState struct {
+	Instance  string
+	Fallback  string
+	Timestamp int64
+	Working   bool
 }
 
-func fetchServiceList() error {
+var lastChecked int64
+var serviceStates map[string][]ServiceState
+
+func load() {
 	resp, err := http.Get("https://raw.githubusercontent.com/benbusby/farside/main/services-full.json")
 	if err != nil {
-		return err
+		fmt.Println(err)
+		return
 	}
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return err
+		fmt.Println(err)
+		return
 	}
 
-	err = json.Unmarshal(body, &services)
+	var serviceList []ServiceType
+	err = json.Unmarshal(body, &serviceList)
 	if err != nil {
-		return err
+		fmt.Println(err)
+		return
 	}
 
-	return nil
-}
+	serviceStates = make(map[string][]ServiceState)
 
-func checkServiceStates() error {
-	fmt.Println("[main] Checking services...")
+	for _, service := range serviceList {
+		serviceStates[service.Type] = []ServiceState{}
 
-	for _, sv := range services {
-		go func(sv Service) {
-			var serviceAvailability []struct {
-				Server    string `json:"server"`
-				Available bool   `json:"available"`
-			}
-
-			for _, insMultiUrls := range sv.Instances {
-				insMultiAvailability := true
-
-				insMultiUrlsArr := strings.Split(insMultiUrls, "|")
-				for _, insSingleUrl := range insMultiUrlsArr {
-					response, err := fetchWithTimeout(fmt.Sprintf("%s/%s", insSingleUrl, sv.TestURL), 3*time.Second)
-					if err != nil || strings.HasPrefix(response.Status, "5") {
-						insMultiAvailability = false
-						break
+		go func(service ServiceType) {
+			for _, instance := range service.Instances {
+				for _, subInstance := range strings.Split(instance, "|") {
+					if strings.HasSuffix(subInstance, ".onion") {
+						continue
 					}
-				}
 
-				if insMultiAvailability {
-					serviceAvailability = append(serviceAvailability, struct {
-						Server    string `json:"server"`
-						Available bool   `json:"available"`
-					}{
-						Server:    insMultiUrls,
-						Available: true,
-					})
-				} else {
-					serviceAvailability = append(serviceAvailability, struct {
-						Server    string `json:"server"`
-						Available bool   `json:"available"`
-					}{
-						Server:    insMultiUrls,
-						Available: false,
+					serviceStates[service.Type] = append(serviceStates[service.Type], ServiceState{
+						Instance:  subInstance,
+						Fallback:  service.Fallback,
+						Timestamp: time.Now().Unix(),
+						Working:   checkWorking(subInstance, service.TestURL),
 					})
 				}
 			}
 
-			fmt.Printf("[%s] Available: %d/%d\n", sv.Type, len(serviceAvailability), len(sv.Instances))
-
-			serviceStates[sv.Type] = serviceAvailability
-		}(sv)
+			fmt.Printf("[%s] %d/%d\n", service.Type, len(serviceStates[service.Type]), len(service.Instances))
+		}(service)
 	}
 
-	return nil
+	lastChecked = time.Now().Unix()
 }
 
-func renderEmptyResponseBody() string {
-	var emptyResponseBody []string
+func checkWorking(instance string, testURL string) bool {
+	client := &http.Client{Timeout: 3 * time.Second}
 
-	for svType, svStates := range serviceStates {
-		var svInstances []string
-
-		for _, ins := range svStates {
-			svInstances = append(svInstances, fmt.Sprintf("- <a href=\"%s\" style=\"color:%s\">%s</a>", ins.Server, func() string {
-				if ins.Available {
-					return "green"
-				}
-				return "red"
-			}(), ins.Server))
-		}
-
-		emptyResponseBody = append(emptyResponseBody, fmt.Sprintf("<h3>%s</h3>\n%s", svType, strings.Join(svInstances, "<br/>")))
+	resp, err := client.Get(instance + testURL)
+	if err != nil {
+		return false
 	}
+	defer resp.Body.Close()
 
-	return strings.Join(emptyResponseBody, "\n")
+	return resp.StatusCode == 200
 }
 
 func main() {
-	serviceStates = make(map[string][]struct {
-		Server    string `json:"server"`
-		Available bool   `json:"available"`
-	})
-
-	err := fetchServiceList()
-	if err != nil {
-		fmt.Println("Failed to fetch service list:", err)
-		return
-	}
-
-	err = checkServiceStates()
-	if err != nil {
-		fmt.Println("Failed to check service states:", err)
-		return
-	}
-
-	renderEmptyResponseBody()
-
+	go load()
 	go func() {
-		for range time.Tick(5 * time.Minute) {
-			err := checkServiceStates()
-			if err != nil {
-				fmt.Println("Failed to check service states:", err)
-			}
+		for range time.Tick(time.Duration(300) * time.Second) {
+			load()
 		}
 	}()
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		url := r.URL
-		requestService := strings.Split(url.Path, "/")[1]
-		var matchingService string
+		urlService := strings.Split(url.Path[1:], "/")[0]
+		urlPath := url.Path[len(urlService)+1:]
 
-		for _, sv := range services {
-			if sv.Type == requestService {
-				matchingService = sv.Type
-				break
-			}
-		}
-
-		if matchingService != "" {
-			var availableServices []struct {
-				Server    string `json:"server"`
-				Available bool   `json:"available"`
-			}
-
-			for _, sv := range serviceStates[matchingService] {
-				if sv.Available {
-					availableServices = append(availableServices, sv)
-				}
-			}
-
-			if len(availableServices) > 0 {
-				availableService := availableServices[rand.Intn(len(availableServices))]
-				urlQueryString := url.Query().Encode()
-				if len(urlQueryString) > 0 {
-					urlQueryString = "?" + urlQueryString
-				}
-				http.Redirect(w, r, fmt.Sprintf("%s%s%s", availableService.Server, url.Path[len(matchingService)+1:], urlQueryString), http.StatusMovedPermanently)
-			} else {
-				w.WriteHeader(http.StatusBadRequest)
-				w.Write([]byte(`{"status": 400, "message": "No server available to redirect"}`))
-			}
-		} else {
+		if serviceStates[urlService] == nil {
 			w.Header().Set("Content-Type", "text/html")
-			w.Write([]byte(renderEmptyResponseBody()))
+			fmt.Fprintf(w, "<div><i>Last checked: %s</i></div>", time.Unix(lastChecked, 0).UTC().Format(time.RFC1123Z))
+			for key := range serviceStates {
+				fmt.Fprintf(w, "<h2>%s</h2>", key)
+				for _, state := range serviceStates[key] {
+					color := "red"
+					if state.Working {
+						color = "blue"
+					}
+					fmt.Fprintf(w, "<div><a style=\"color:%s\" href=\"%s\"><b>%s</b></a></div>", color, state.Instance, state.Instance)
+				}
+			}
+			return
 		}
+
+		service := serviceStates[urlService]
+		workingServices := make([]ServiceState, 0)
+		for _, state := range service {
+			if state.Working {
+				workingServices = append(workingServices, state)
+			}
+		}
+
+		randomIndex := rand.Intn(len(workingServices))
+		selectedService := workingServices[randomIndex]
+
+		http.Redirect(w, r, selectedService.Instance+urlPath, http.StatusFound)
 	})
 
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "3000"
-	}
-
-	err = http.ListenAndServe(":"+port, nil)
-	if err != nil {
-		fmt.Println("Failed to start server:", err)
-	}
+	http.ListenAndServe(":31313", nil)
 }
